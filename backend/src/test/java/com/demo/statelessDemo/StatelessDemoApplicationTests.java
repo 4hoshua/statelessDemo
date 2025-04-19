997cdfdab45390763b8b592187763d021261b78b
yanalec <yanalec448@gmail.com>
2025-04-18 21:07:02 -0300

    First commit

---
+++ backend/src/test/java/com/demo/statelessDemo/StatelessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.statelessDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatelessDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
